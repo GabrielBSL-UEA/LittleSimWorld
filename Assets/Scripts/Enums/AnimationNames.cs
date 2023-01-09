07e437cf08dc101826686b6575fbd7829fcee540
GabrielBSL-UEA <69216849+GabrielBSL-UEA@users.noreply.github.com>
2023-01-09 15:43:01 -0400

    Player animation
    Player is animated based on the current velocity.
    As the spritesheet is pixel based, the animation can be called directly by code.
    
    New classes:
    Animations: help translate the desired animation into a string to be played.
    
    New enums:
    AnimationNames
    Directions

---
+++ Assets/Scripts/Enums/AnimationNames.cs
@@ -0,0 +1,15 @@
+namespace Enums
+{
+    //Enum that have all the common animation names
+    public enum AnimationNames
+    {
+        idle_up,
+        idle_down,
+        idle_left,
+        idle_right,
+        walk_up,
+        walk_down,
+        walk_left,
+        walk_right
+    }
+}
---
+++ Assets/Scripts/Enums/Directions.cs
@@ -0,0 +1,11 @@
+namespace Enums
+{
+    //Enum that have the common input directions
+    public enum direction
+    {
+        up,
+        down,
+        left,
+        right
+    }
+}
